00e976864bb6966de270a94d7ab4eda831cbe494
Abhish Alkesh Bondre <148959477+abhishbondre@users.noreply.github.com>
2026-04-12 08:49:54 +0530

    Update API endpoint for task retrieval

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [text, setText] = useState("");
 
-  const API = "http://51.20.188.209:5000/api/tasks";
+  const API = "http://34.227.60.15:5000/api/tasks";
 
   // GET tasks
   const getTasks = async () => {
